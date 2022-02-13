1c8678eabecc108240a201482a69bb866c50821b
Arya <aryanarayantiwari@gmail.com>
2022-02-13 19:05:56 +0530

    add showcase page

---
+++ app.js
@@ -0,0 +1,270 @@
+const data = [
+  {
+    name: "Expanding Cards",
+    href: "Day 01 - Expanding Cards",
+    image: "images/1.png",
+  },
+  {
+    name: "Progress Steps",
+    href: "Day 02 - Progress Steps",
+    image: "images/2.png",
+  },
+  {
+    name: "Rotating Navigation",
+    href: "Day 03 - Rotating Navigation",
+    image: "images/3.png",
+  },
+  {
+    name: "Hidden Search Widget",
+    href: "Day 04 - Hidden Search Widget",
+    image: "images/4.png",
+  },
+  {
+    name: "Blurry Loading",
+    href: "Day 05 - Blurry Loading",
+    image: "images/5.png",
+  },
+  {
+    name: "Scroll Animation",
+    href: "Day 06 - Scroll Animation",
+    image: "images/6.png",
+  },
+  {
+    name: "Split Landing Page",
+    href: "Day 07 - Split Landing Page",
+    image: "images/7.png",
+  },
+  {
+    name: "Form Wave Animation",
+    href: "Day 08 - Form Wave Animation",
+    image: "images/8.png",
+  },
+  {
+    name: "Sound Board",
+    href: "Day 09 - Sound Board",
+    image: "images/9.png",
+  },
+  {
+    name: "Dad Jokes",
+    href: "Day 10 - Dad Jokes",
+    image: "images/10.png",
+  },
+  {
+    name: "Event KeyCodes",
+    href: "Day 11 - Event KeyCodes",
+    image: "images/11.png",
+  },
+  {
+    name: "FAQ Collapse",
+    href: "Day 12 - FAQ Collapse",
+    image: "images/12.png",
+  },
+  {
+    name: "Random Choice Picker",
+    href: "Day 13 - Random Choice Picker",
+    image: "images/13.png",
+  },
+  {
+    name: "Animated Navigation",
+    href: "Day 14 - Animated Navigation",
+    image: "images/14.png",
+  },
+  {
+    name: "Incrementing Counter",
+    href: "Day 15 - Incrementing Counter",
+    image: "images/15.png",
+  },
+  {
+    name: "Drink Water",
+    href: "Day 16 - Drink Water",
+    image: "images/16.png",
+  },
+  {
+    name: "Movies App",
+    href: "Day 17 - Movies App",
+    image: "images/17.png",
+  },
+  {
+    name: "Background Slider",
+    href: "Day 18 - Background Slider",
+    image: "images/18.png",
+  },
+  {
+    name: "Theme Clock",
+    href: "Day 19 - Theme Clock",
+    image: "images/19.png",
+  },
+  {
+    name: "Button Ripple Effect",
+    href: "Day 20 - Button Ripple Effect",
+    image: "images/20.png",
+  },
+  {
+    name: "Drag N Drop",
+    href: "Day 21 - Drag N Drop",
+    image: "images/21.png",
+  },
+  {
+    name: "Drawing App",
+    href: "Day 22 - Drawing App",
+    image: "images/22.png",
+  },
+  {
+    name: "Kinetic CSS Loader",
+    href: "Day 23 - Kinetic CSS Loader",
+    image: "images/23.png",
+  },
+
+  {
+    name: "Content Placeholder",
+    href: "Day 24 - Content Placeholder",
+    image: "images/24.png",
+  },
+  {
+    name: "Sticky Nav",
+    href: "Day 25 - Sticky Nav",
+    image: "images/25.png",
+  },
+  {
+    name: "Double Vertical Slider",
+    href: "Day 26 - Double Vertical Slider",
+    image: "images/26.png",
+  },
+  {
+    name: "Toast Notification",
+    href: "Day 27 - Toast Notification",
+    image: "images/27.png",
+  },
+  {
+    name: "Github Profiles",
+    href: "Day 28 - GitHub Profiles",
+    image: "images/28.png",
+  },
+  {
+    name: "Double Heart Click",
+    href: "Day 29 - Double Heart Click",
+    image: "images/29.png",
+  },
+  {
+    name: "Auto Text Effect",
+    href: "Day 30 - Auto Text Effect",
+    image: "images/30.png",
+  },
+  {
+    name: "Password Generator",
+    href: "Day 31 - Password Generator",
+    image: "images/31.png",
+  },
+  {
+    name: "Good Cheap Fast Checkboxes",
+    href: "Day 32 - Good Cheap Fast Checkboxes",
+    image: "images/32.png",
+  },
+  {
+    name: "Notes App",
+    href: "Day 33 - Notes App",
+    image: "images/33.png",
+  },
+  {
+    name: "Animated Countdown",
+    href: "Day 34 - Animated Countdown",
+    image: "images/34.png",
+  },
+  {
+    name: "Image Carousel",
+    href: "Day 35 - Image Carousel",
+    image: "images/35.png",
+  },
+  {
+    name: "Hoverboard",
+    href: "Day 36 - Hoverboard",
+    image: "images/36.png",
+  },
+  {
+    name: "Pokedex",
+    href: "Day 37 - Pokedex",
+    image: "images/37.png",
+  },
+  {
+    name: "Mobile Tab Navigation",
+    href: "Day 38 - Mobile Tab Navigation",
+    image: "images/38.png",
+  },
+  {
+    name: "Password Strength Background",
+    href: "Day 39 - Password Strength Background",
+    image: "images/39.png",
+  },
+  {
+    name: "3D Boxes Background",
+    href: "Day 40 - 3D Boxes Background",
+    image: "images/40.png",
+  },
+  {
+    name: "Verify Account UI",
+    href: "Day 41 - Verify Account UI",
+    image: "images/41.png",
+  },
+  {
+    name: "Live user Filter",
+    href: "Day 42 - Live user Filter",
+    image: "images/42.png",
+  },
+  {
+    name: "Feedback UI Design",
+    href: "Day 43 - Feedback UI Design",
+    image: "images/43.png",
+  },
+  {
+    name: "Custom Range Slider",
+    href: "Day 44 - Custom Range Slider",
+    image: "images/44.png",
+  },
+  {
+    name: "Netflix Mobile Navigation",
+    href: "Day 45 - Netflix Mobile Navigation",
+    image: "images/45.png",
+  },
+  {
+    name: "Quiz App",
+    href: "Day 46 - Quiz App",
+    image: "images/46.png",
+  },
+  {
+    name: "Testimonial Box Switcher",
+    href: "Day 47 - Testimonial Box Switcher",
+    image: "images/47.png",
+  },
+  {
+    name: "Random Image Feed",
+    href: "Day 48 - Random Image Feed",
+    image: "images/48.png",
+  },
+  {
+    name: "Todo List",
+    href: "Day 49 - Todo List",
+    image: "images/49.png",
+  },
+  {
+    name: "Insect Catch Game",
+    href: "Day 50 - Insect Catch Game",
+    image: "images/50.png",
+  }
+];
+
+const container = document.querySelector(".container");
+
+data.forEach((elem) => {
+  const card = document.createElement("div");
+  card.innerHTML = `
+    <a href="${elem["href"]}" target="_blank">
+              <span class="day">Day </span>
+              <img src="${elem["image"]}" alt="card" />
+              <div class="card-info">
+                <h2>${elem["name"]}</h2>
+              </div>
+            </a>
+    `;
+  card.classList.add("card");
+  container.appendChild(card);
+});
